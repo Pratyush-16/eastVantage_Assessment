5e3e57d813cae9117a8db1704ea2f1e07dec21f7
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2024-02-04 00:14:39 +0530

    style added

--- userInfo/src/App.jsx
+++ userInfo/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       )}
       <button
         onClick={handleRefresh}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
       >
         Refresh
       </button>
