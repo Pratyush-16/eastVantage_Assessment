84cab84e1b840778d1c9f5295b462ee43de97af7
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2024-02-03 23:35:50 +0530

    functionality done

--- userInfo/src/App.jsx
+++ userInfo/src/App.jsx
@@ -1,12 +1,51 @@
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
+  
+  const [userData, setUserData] = useState(0)
+
+  const fetchdata = async() => {
+    try{
+      const response = await axios.get('https://randomuser.me/api');
+      const { name, email } = response.data.results[0];
+
+      localStorage.setItem('user', JSON.stringify({ name, email }));
+
+      setUserData({ name, email });
+
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(()=> {
+    fetchdata()
+  },[])
+
+  const handleRefresh = () => {
+    // Refresh the component by fetching new user data
+    fetchdata();
+  };
+
   
 
   return (
     <>
      <h1>User Info</h1>
+
+     {userData ? (
+        <div>
+          <h1>{`${userData.name.title} ${userData.name.first} ${userData.name.last}`}</h1>
+          <p>Email: {userData.email}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={handleRefresh}>Refresh</button>
+    
     </>
   )
 }
