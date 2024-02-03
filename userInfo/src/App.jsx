690c908d7dac654978772501f39e78677ca5f245
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2024-02-04 00:30:50 +0530

    changes updated

--- userInfo/src/App.jsx
+++ userInfo/src/App.jsx
@@ -34,7 +34,8 @@ function App() {
 
   return (
     <div className="text-center my-8">
-      <h1 className='text text-6xl  my-8'>User Info</h1>
+
+      <h1 className='text-6xl my-8'>User Info</h1>
       {userData ? (
         <div>
           <h1 className="text-4xl font-bold mb-2">
@@ -47,7 +48,7 @@ function App() {
       )}
       <button
         onClick={handleRefresh}
-        className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
       >
         Refresh
       </button>
